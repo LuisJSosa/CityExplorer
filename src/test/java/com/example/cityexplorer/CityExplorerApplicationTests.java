658066a9565897aa42b165008b9e81360c91ee7b
Luis Sosa <94214924+LuisJSosa@users.noreply.github.com>
2024-02-15 03:02:24 -0500

    commiting started repo

---
+++ src/test/java/com/example/cityexplorer/CityExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cityexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityExplorerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
